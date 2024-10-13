d71e5082d4c14f27dee48e01ddc4b7b34ec86871
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-13 19:34:37 +0000

    Update functions.h

--- functions.h
+++ functions.h
@@ -7,6 +7,7 @@ struct Node {
     struct Node* right;
     struct Node* parent; //kontrol için kullanışlı
 };
+struct Node* newNode(int data, struct Node* parent)
 void printArray(int A[], int size);
 
 #endif
