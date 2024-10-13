ae85d3621a90fed54b5b1bd14d61834bfa34c98c
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-13 19:21:58 +0000

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,6 +1,12 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+    struct Node* parent; //kontrol için kullanışlı
+};
 void printArray(int A[], int size);
 
 #endif
