c752eede2b9758e950aa6ef5631ba401e82bce51
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-10-13 19:33:34 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -6,3 +6,12 @@ void printArray(int A[], int size) {
         printf("%d ", A[i]);
     printf("\n");
 }
+
+struct Node* newNode(int data, struct Node* parent) {
+    struct Node* node = malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    node->parent = parent;
+    return node;
+}
