8edac591fc0a0fd3001d6fe0133a9b5ebe8798ea
fc63 <furkan@coban.email>
2024-10-18 19:22:09 +0300

    bir takım format değişiklikleri

--- bubbleSort.c
+++ bubbleSort.c
@@ -5,9 +5,9 @@ static int arr[] = {64,34,25,12,22,11,90};
 void bubbleSort(){
     int i, j;
     bool swapped;
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < ARSZ(arr) - 1; i++) {
         swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
+        for (j = 0; j < ARSZ(arr) - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(&arr[j], &arr[j + 1]);
                 swapped = true;
