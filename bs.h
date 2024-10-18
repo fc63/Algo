45073351b9518f31538b4470fe29f8cb0c0c88b7
fc63 <furkan@coban.email>
2024-10-18 19:21:32 +0300

    bir takım format değişiklikleri

--- bs.h
+++ bs.h
@@ -2,6 +2,6 @@
 #define BS_H
 
 int bs();
-void bubbleSort(int n);
+void bubbleSort();
 
 #endif //BS_H
--- bubbleSort.c
+++ bubbleSort.c
@@ -2,7 +2,7 @@
 
 static int arr[] = {64,34,25,12,22,11,90};
 
-void bubbleSort(int n){
+void bubbleSort(){
     int i, j;
     bool swapped;
     for (i = 0; i < n - 1; i++) {
@@ -24,7 +24,7 @@ int bs() {
     printf("Given array is \n");
     printArray(arr, ARSZ(arr));
 
-    bubbleSort(ARSZ(arr));
+    bubbleSort();
 
     printf("\nSorted array is \n");
     printArray(arr,ARSZ(arr));
